6b5e4e7988f19652876adda1ad10e6c2c7aa1393
Nicolás Hechim <nicolashechim@gmail.com>
2017-05-31 19:45:40 +0200

    Enumerado para celdas de una fiesta.

---
+++ Fiestapp/Fiestapp/EnumFiestasCell.swift
@@ -0,0 +1,20 @@
+//
+//  EnumFiestasCell.swift
+//  Fiestapp
+//
+//  Created by Nicolás Hechim on 31/5/17.
+//  Copyright © 2017 Mint. All rights reserved.
+//
+
+import UIKit
+
+enum EnumFiestasCell: String {
+    case Informacion = "InformacionCell",
+    Galeria = "GaleriaCell",
+    MeGustas = "MeGustasCell",
+    Menu = "MenuCell",
+    Regalos = "ListaRegalosCell",
+    Asistencia = "AsistenciaCell",
+    Vestimenta = "VestimentaCell",
+    Musica = "MusicaCell"
+}
